4223cb4372fff9f5e792ff99cc70e794615e53fe
FalkenS <steffalkenburg@gmail.com>
2013-12-01 13:37:39 +0100

    factuur

--- BackupBackup/src/model/detailModel/Factuur.java
+++ BackupBackup/src/model/detailModel/Factuur.java
@@ -17,10 +17,31 @@ public class Factuur extends JFrame{
 		this.detailModel = detailModel;
 		onderdelen = detailModel.getGebruikteOnderdelen();
 		
-		gegevensA = new JTextArea("Garage\n Van Galenstraat 19\n 7511 JL Enschede\n 053-123456\n\n" + 
-				"ditiseenemailadres@geenemail.com\n IBAN: NL00GASF0112233445\n BIC: GASFNL4W\n KvK Overijssel 75341286\n BTW NL777881145A99\n\n");
+		gegevensA = new JTextArea("Garage\nVan Galenstraat 19\n7511 JL Enschede\n053-123456\n\n" + 
+				"ditiseenemailadres@geenemail.com\nIBAN: NL00GASF0112233445\nBIC: GASFNL4W\nKvK Overijssel 75341286\nBTW NL777881145A99\n\n");
 		gegevensA.setEditable(false);
 		
+		regelsToevoegen();
+		onderdelenToevoegen();
+	}
+	
+	public void regelsToevoegen(){
+		gegevensA.append("Factuurnummer: \n");
+		gegevensA.append("Factuurdatum: \n");
+		gegevensA.append("Reparatienummer: " + detailModel.getReparatieID() + "\n");
+		gegevensA.append("Klantnummer: " + detailModel.getPersoonID() + "\n");
+		gegevensA.append("Autonummer: " + detailModel.getAutoID()+ "\n\n");
 	}
 
+	public void onderdelenToevoegen(){
+		gegevensA.append(" Onderdeelnummer	Onderdeelnaam	Leverancier	 Prijs	Aantal	Totaalprijs\n\n");
+		for(int i=0; i < onderdelen.length; i++){
+			for(int j=0; j < onderdelen[i].length; j++){
+				gegevensA.append("" + onderdelen[i][j] + "	");
+				if(j == (onderdelen[i].length-1)){
+					gegevensA.append("\n");
+				}
+			}
+		}
+	}
 }
